b5fd8691394b8ccc77be856c678792f49db99a43
Sludovic <Sludovic@users.noreply.github.com>
2015-03-17 16:55:36 +0100

    Update ExportToCsvBasic.js

--- ExportToCsvBasic.js
+++ ExportToCsvBasic.js
@@ -31,14 +31,3 @@ function exportToCsv(link,JSONdata,KEYSdata,name,HeaderData) {
     link.setAttribute("href", encodeURI(csvContent));
     link.setAttribute("download", name);    
 }
-
-Ex : 
-```HTML
-<script>
-    var headercsv = ["Nom","Total", "MAJ", "Nouveau"];
-    var headerdata = ["Nom", "Nombre Valide", "Mis a jour", "Nouveaux"];
-    var datacsv = '[{"Nom":"la première ligne","MAJ":"0","Nouveau":"5475","Total":"5475"},{"Nom":"le deuxieme ligne","MAJ":"60","Nouveau":"0","Total":"60"}]';
-    var name = 'tableau.csv';
-</script>
-<a onClick="exportToCsv(this,datacsv,headercsv,name,headerdata);">Télécharger le tableau</a>
-```
